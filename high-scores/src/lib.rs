ad03154d55b2a5b0603e1eb6b501890253f490c3
Jakub Matys <jakub.matys@showmax.com>
2020-04-26 01:03:44 +0200

    high-scores

---
+++ high-scores/src/lib.rs
@@ -0,0 +1,28 @@
+#[derive(Debug)]
+pub struct HighScores<'a> {
+    scores: &'a [u32],
+}
+
+impl<'a> HighScores<'a> {
+    pub fn new(scores: &'a [u32]) -> Self {
+        HighScores { scores: scores }
+    }
+
+    pub fn scores(&self) -> &'a [u32] {
+        self.scores
+    }
+
+    pub fn latest(&self) -> Option<u32> {
+        self.scores.iter().last().copied()
+    }
+
+    pub fn personal_best(&self) -> Option<u32> {
+        self.scores.iter().max().copied()
+    }
+
+    pub fn personal_top_three(&self) -> Vec<u32> {
+        let mut v = self.scores.to_vec();
+        v.sort();
+        v.iter().rev().take(3).copied().collect()
+    }
+}
