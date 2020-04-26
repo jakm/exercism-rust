746b1dc6674a24504964871d307fccc8eb62debb
Jakub Matys <jakub.matys@showmax.com>
2020-04-27 00:49:52 +0200

    bob #2

--- bob/src/lib.rs
+++ bob/src/lib.rs
@@ -3,8 +3,7 @@ pub fn reply(message: &str) -> &str {
     if s.is_empty() {
         return "Fine. Be that way!";
     }
-    let alpha: Vec<char> = s.chars().filter(|c| c.is_alphabetic()).collect();
-    let yelling = !alpha.is_empty() && alpha.iter().all(|c| c.is_uppercase());
+    let yelling = s.matches(char::is_alphabetic).count() > 0 && s.to_uppercase() == s;
     let questioning = s.chars().last() == Some('?');
     match (questioning, yelling) {
         (true, true) => "Calm down, I know what I'm doing!",
